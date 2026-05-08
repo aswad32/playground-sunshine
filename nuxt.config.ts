583aabb1cecf05ca19bdeedb9a7f7875e1727271
aswad32 <aswad32@gmail.com>
2026-05-08 21:57:35 +0800

    chore: add Vercel Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vercel/analytics'],
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
   },
